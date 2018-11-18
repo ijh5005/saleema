eaa60a4809a920d65d6336e23c99880e94210f70
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2018-11-18 11:00:05 -0500

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('app', []);
+
+app.controller('ctrl', ['$scope', '$rootScope', '$interval', '$timeout', 'task', function($scope, $rootScope, $interval, $timeout, task){
+
+
+}]);
+
+
+app.service('task', function($rootScope, $interval, $timeout){
+
+});
